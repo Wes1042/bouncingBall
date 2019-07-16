5bdb9626ed192ff19af90d2f50f2be76792fdd30
WesleyASC6 <wesleytrujillo@uagateway.org>
2019-07-16 10:31:22 -0400

    not done

---
+++ sketch4.js
@@ -0,0 +1,17 @@
+function setup(){
+    createCanvas(600,600);
+    background(255,0,0)
+
+
+}
+    
+
+let posX = 0;
+let posY = 0;
+function draw(){
+background(25,0,0);
+    ellipse(posX,posY,60,60)
+    //noStroke();
+posX++
+posY ++
+}
